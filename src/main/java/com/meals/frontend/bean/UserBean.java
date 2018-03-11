8fe15ff1fa3aea55bb2c323774495c6adf42f747
cong268 <congnv.hust@gmail.com>
2018-03-11 22:50:02 +0700

    add

---
+++ src/main/java/com/meals/frontend/bean/UserBean.java
@@ -0,0 +1,59 @@
+package com.meals.frontend.bean;
+
+public class UserBean {
+	private Integer userId;
+	private String userRole;
+	private Integer staffId;
+	private Boolean disable;
+	private String userName;
+	private String password;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	public Integer getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(Integer staffId) {
+		this.staffId = staffId;
+	}
+
+	public Boolean getDisable() {
+		return disable;
+	}
+
+	public void setDisable(Boolean disable) {
+		this.disable = disable;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
