b8437f8790a8ea4f82ad0c375322b09f9ef4a6f8
cong268 <congnv.hust@gmail.com>
2018-03-13 23:08:49 +0700

    add

--- src/main/java/com/meals/backend/dao/CateringDAO.java
+++ src/main/java/com/meals/backend/dao/CateringDAO.java
@@ -7,5 +7,6 @@ import com.meals.backend.model.Catering;
 
 public interface CateringDAO {
 	List<Catering> getLstByDepartment(List<String> staffIds, Date date);
+	List<Catering> getLstAppro (List<String> staffLst, Date cateringDate);
 	Boolean saveOrUpdate(Catering obj);
 }
