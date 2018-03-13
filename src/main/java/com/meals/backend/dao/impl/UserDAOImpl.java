f6895eb22ea5492c5c339f6d09b6e3c97510a0c1
chientd <chienpc122@gmail.com>
2018-03-13 23:29:39 +0700

    revert

--- src/main/java/com/meals/backend/dao/impl/UserDAOImpl.java
+++ src/main/java/com/meals/backend/dao/impl/UserDAOImpl.java
@@ -24,7 +24,8 @@ public class UserDAOImpl implements UserDAO {
 
 	public User getUser(String userName, String passWord) {
 		Session session = sessionFactory.getCurrentSession();
-2		query.setParameter("userName", userName);
+		Query query = session.createQuery("FROM User c WHERE c.userName = :userName AND c.password = :password");
+		query.setParameter("userName", userName);
 		query.setParameter("password", passWord);
 		@SuppressWarnings("unchecked")
 		List<User> list = query.list();
